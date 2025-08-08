d6ec01ab89f027cfe3d43c5ba33d090c7d463074
MykolaBakosh <mykolabakos@gmail.com>
2025-08-08 21:34:57 +0300

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import css from "./App.module.css";
+import { type Votes, type VoteType } from "../../types/votes.ts";
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import VoteStats from "../VoteStats/VoteStats.tsx";
+import Notification from "../Notification/Notification.tsx";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({ ...prevVotes, [type]: prevVotes[type] + 1 }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate =
+    totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App/App.tsx'
+import "modern-normalize";
+import './index.css'
+
+
+createRoot(document.getElementById('root')as HTMLDivElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
